ff919ec75c654baec96ef2fee3ee464bb72e00ca
azkamalaz <kamalazenag@gmail.com>
2024-01-20 21:16:52 +0100

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,15 @@
 # 1-install_a_package.pp
 
-# Define the package resource for flask
-package { 'flask':
+class { 'python':
+  version => 'system',
+}
+
+class { 'python::pip':
+  version => 'system',
+}
+
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  install_options => ['-I'],  # Ignore installed version and force installation
+  require  => Class['python::pip'],
 }
