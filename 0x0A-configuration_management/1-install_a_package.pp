5c41000601d40b721d5847c4cf29c3d18037a2ac
azkamalaz <kamalazenag@gmail.com>
2024-01-20 21:13:16 +0100

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,5 @@
-# 1-install_a_package.pp
-
-# Define the package resource for flask
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
-}
-
-# Notify the user when the package is installed
-notify { 'flask installed successfully':
-  require => Package['flask'],
+  provider => 'pip',
+  install_options => ['-I'],  # Ignore installed version and force installation
 }
