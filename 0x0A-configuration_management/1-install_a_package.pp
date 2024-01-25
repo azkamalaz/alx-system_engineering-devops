0d037f55e4ca6174bc01f3e45e4175576516a4a0
Your Nameazkamalaz <kamalazenag@gmail.com>
2024-01-25 07:49:30 -0800

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,21 +1,7 @@
-# 1-install_a_package.pp
-
-class { 'python':
-  version => 'system',
-}
-
-class { 'python::pip':
-  version => 'system',
-}
-
-package { 'Flask':
+#!/usr/bin/pup
+# Install an especific version of flask (2.1.0)
+package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
-  require  => Class['python::pip'],
+  provider => 'pip3'
 }
 
-package { 'Werkzeug':
-  ensure   => '2.0.2',
-  provider => 'pip3',
-  require  => Class['python::pip'],
-}
