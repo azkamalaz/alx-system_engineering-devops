b21c28de5cc7d39fb844661de01da5872a372701
azkamalaz <kamalazenag@gmail.com>
2024-03-05 23:50:29 +0100

    0x16-api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,31 +5,15 @@ Script that queries subscribers on a given Reddit subreddit.
 
 import requests
 
+
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "Custom User Agent"}
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        response.raise_for_status()  # Raise an exception for 4xx or 5xx errors
-        if response.status_code == 200:
-            data = response.json()
-            subscribers = data['data']['subscribers']
-            return subscribers
-        else:
-            print(f"Failed to retrieve data for subreddit: {subreddit}, Status Code: {response.status_code}")
-            return 0
-    except requests.exceptions.HTTPError as err:
-        print(f"HTTP error occurred: {err}")
-        return 0
-    except Exception as err:
-        print(f"An unexpected error occurred: {err}")
-        return 0
-
-if __name__ == "__main__":
-    import sys
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
     else:
-        subreddit = sys.argv[1]
-        print(number_of_subscribers(subreddit))
+        return 0
